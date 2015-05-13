49dedbf23686da402957dba867562fb3a57d9ace
Ben Goertz <bengoertz@gmail.com>
2015-05-13 16:16:21 -0700

    Microphone button how shows the recording label once you tap it.

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(sender: UIButton) {
+        println("Yo!")
+        // TODO: Record audio.
+        
+        // Show label when recording.
+        recordLabel.hidden = false
+    }
+    
 }
 
