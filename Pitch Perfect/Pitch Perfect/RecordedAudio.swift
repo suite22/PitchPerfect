0d3d47dc92232fe8d99e7c1e8d54ee05b33aa90e
Ben <bengoertz@gmail.com>
2015-07-10 20:11:53 -0700

    Added a new class to the project.

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Ben Goertz on 7/10/15.
+//  Copyright (c) 2015 Ben Goertz. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
