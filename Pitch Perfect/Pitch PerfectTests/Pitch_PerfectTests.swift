1d11d1b7b1a480f7ff1a51a04ae9a06337dd045c
suite22 <bengoertz@gmail.com>
2015-10-31 09:37:52 -0700

    Initial testing for the RecordSoundsVC.

--- Pitch Perfect/Pitch PerfectTests/Pitch_PerfectTests.swift
+++ Pitch Perfect/Pitch PerfectTests/Pitch_PerfectTests.swift
@@ -9,11 +9,21 @@
 import UIKit
 import XCTest
 
+@testable import Pitch_Perfect
+
 class Pitch_PerfectTests: XCTestCase {
-    
+	
+	var recordVC: RecordSoundViewController!
+	
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+		let storyboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: self.dynamicType))
+		self.recordVC = storyboard.instantiateViewControllerWithIdentifier("Record") as! RecordSoundViewController
+		
+		let _ = recordVC.view
+		
+		self.recordVC.viewDidLoad()
     }
     
     override func tearDown() {
@@ -25,12 +35,15 @@ class Pitch_PerfectTests: XCTestCase {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+	func testRecordVC(){
+		//XCTAssertEqual(recordVC.stopButton.hidden, true, "The stop recording button should be hidden")
+		XCTAssertEqual(recordVC.recordLabel.hidden, true, "The recording label should be hidden")
+		XCTAssertEqual(recordVC.recordButton.enabled, true, "The recording button should be enabled")
+		
+		recordVC.recordAudio(recordVC.recordButton)
+		XCTAssertEqual(recordVC.stopButton.hidden, false, "The stop recording button should be visible")
+		XCTAssertEqual(recordVC.recordLabel.hidden, false, "The recording label should be visible")
+		XCTAssertEqual(recordVC.recordButton.enabled, false, "The record button should be disabled")
+	}
 }
